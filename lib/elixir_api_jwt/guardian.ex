a03383b6e2cbc255d5a5bc67da96fe06dcbf5321
mik284 <mikeykamau222@gmail.com>
2024-02-19 08:32:24 +0300

    ft: implemented resource_from_claims/1 function

--- lib/elixir_api_jwt/guardian.ex
+++ lib/elixir_api_jwt/guardian.ex
@@ -9,4 +9,22 @@ defmodule ElixirApiJwt.Guardian do
     sub = to_string(id)
     {:ok, sub}
   end
+  def subject_for_token(_, _) do
+    {:error, :reason_for_error}
+  end
+
+  def resource_from_claims(%{"sub"=>id}) do
+    # Here we'll look up our resource from the claims, the subject can be
+    # found in the `"sub"` key. In above `subject_for_token/2` we returned
+    # the resource id so here we'll rely on that to look it up.
+
+    case ElixirApiJwt.Accounts.get_account!(id) do
+      nil -> {:error, :reason_for_error}
+      resource -> {:ok, resource}
+    end
+  end
+
+  def resource_from_claims(_claims) do
+      {:error, :reason_for_error}
+  end
 end
