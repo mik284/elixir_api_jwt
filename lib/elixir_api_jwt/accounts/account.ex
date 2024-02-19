6077799d2aa8e48aa222297a3f0e9888992ddea2
mik284 <mikeykamau222@gmail.com>
2024-02-19 08:03:09 +0300

    ft: added hashed_password

--- lib/elixir_api_jwt/accounts/account.ex
+++ lib/elixir_api_jwt/accounts/account.ex
@@ -17,5 +17,13 @@ defmodule ElixirApiJwt.Accounts.Account do
     |> unique_constraint(:email)
     |> validate_format(:email, ~r/@/)
     |> validate_length(:hash_password, min: 6)
+    |> put_password_hash()
   end
+
+
+  defp put_password_hash(%Ecto.Changeset{valid?: true, changes: %{hash_password: hash_password}}= changeset) do
+
+    change(changeset, hash_password: Bcrypt.hash_pwd_salt(hash_password))
+  end
+  defp put_password_hash(changeset), do: changeset
 end
