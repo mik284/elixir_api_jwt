7dfc3eae58dfb440527b1f30cf097c79d21e9d25
mik284 <mikeykamau222@gmail.com>
2024-02-19 08:27:09 +0300

    ft:implements the subject_for_token/2

---
+++ lib/elixir_api_jwt/guardian.ex
@@ -0,0 +1,12 @@
+defmodule ElixirApiJwt.Guardian do
+  use Guardian, otp_app: :elixir_api_jwt
+  def subject_for_token(%{id: id}, _claims) do
+    # You can use any value for the subject of your token but
+    # it should be useful in retrieving the resource later, see
+    # how it being used on `resource_from_claims/1` function.
+    # A unique `id` is a good subject, a non-unique email address
+    # is a poor subject.
+    sub = to_string(id)
+    {:ok, sub}
+  end
+end
