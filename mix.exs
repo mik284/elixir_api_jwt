360befcd6452dab7649ffb6ea15e20f02ef810ee
mik284 <mikeykamau222@gmail.com>
2024-02-19 08:27:01 +0300

    ft: add guardian

--- mix.exs
+++ mix.exs
@@ -51,7 +51,8 @@ defmodule ElixirApiJwt.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:plug_cowboy, "~> 2.5"},
-      {:bcrypt_elixir, "~> 3.0"}
+      {:bcrypt_elixir, "~> 3.0"},
+      {:guardian, "~> 2.0"}
     ]
   end
 
